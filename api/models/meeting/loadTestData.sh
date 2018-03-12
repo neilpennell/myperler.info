f66071d452ddf49aa85a76ecced14d2c60e32a6c
Neil Pennell <neil.pennell@gmail.com>
2018-03-12 09:07:08 -0500

    updated test generator

--- api/models/meeting/loadTestData.sh
+++ api/models/meeting/loadTestData.sh
@@ -4,11 +4,12 @@ BASE_URL=https://api.myperler.info/meetings-v1/meetings
 API_KEY='--header x-api-key:ljOnBxNfWY2c7zz1VNS7R9Gj604K8bIC2AuiizDf'
 
 echo "Load data"
-curl -X POST $BASE_URL --data '{"title":"meeting 1", "date_occured":"3/4/18"}' $API_KEY
+D=`date +'%s'`
+curl -X POST $BASE_URL --data '{"title":"meeting 1", "date_occured":"'$D'", "attendees":["a1","a2","a3"], "discussions":["d1","d2","d3"], "decisions":["D1","D2","D3"], "actions":["a1","a2","a3"]}' $API_KEY
 curl -X POST $BASE_URL --data '{"title":"meeting 2"}' $API_KEY
 curl -X POST $BASE_URL --data '{"title":"meeting 3"}' $API_KEY
 curl -X POST $BASE_URL --data '{"title":"meeting 4"}' $API_KEY
 
 echo "dump of data"
-echo curl -X GET $BASE_URL $API_KEY
+curl -X GET $BASE_URL $API_KEY
 
